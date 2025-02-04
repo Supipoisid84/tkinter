fb2bbbf7be54dc1928e9931240420e2eba4447a8
Alex Kreimann <alex@hkhk.edu.ee>
2025-02-04 11:43:48 +0200

    Create tikihiki01.py

---
+++ tikihiki01.py
@@ -0,0 +1,11 @@
+#Alex Kreimann
+#04.02.2025
+#tk02
+
+    #Veendu, et arvutis on Python paigaldatud
+    #Vali oma programm ja loo uus projekt tkinter ülesannete jaoks (ära miksi kataloogi eelmiste ülesannetega)
+    #Seadista Github või Github Desktop
+    #Loo rakenduse põhistruktuur, mis käivitab akna 400×400
+    #    akna nimeks pane oma nimi, näiteks “Mario ülesanded”
+    #    keela akna suurendamine y-suunas, x-suunas on lubatud
+    #Käivita aken
