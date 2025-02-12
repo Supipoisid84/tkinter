bd8d2b48a388263f48c754e5a66cacd0f703fad9
Alex Kreimann <alex@hkhk.edu.ee>
2025-02-12 10:01:22 +0200

    Update tikihiki03.py

--- tikihiki03.py
+++ tikihiki03.py
@@ -12,20 +12,31 @@ def main():
     def kuva_sisestus():
         tekst1=sisestus1.get()  # Võtab esimese sisestuse
         tekst2=sisestus2.get()  # Võtab teise sisestuse
-        vastus=tk.Label(aken,text=f"Esimene sisestus: {tekst1},Teine sisestus: {tekst2}")
+        # vastus = tk.Label(aken, text=f"Esimene sisestus: {tekst1}, Teine sisestus: {tekst2}")
+        vastus=tk.Label(aken,text=f"Igakuine makse: {tekst1}")
         vastus.pack()
+    
 
     # Esimene sisestusväli
+    label=tk.Label(aken,text="Laenusumma (€)").pack()
     sisestus1=tk.Entry(aken)
     sisestus1.pack()
    
     # Teine sisestusväli
+    label=tk.Label(aken,text="Aastane intressimäär (%)").pack()
     sisestus2=tk.Entry(aken)
     sisestus2.pack()
+
+    # Kolmas sisestusväli
+    label=tk.Label(aken,text="Laenuperiood (aastad)").pack()
+    sisestus3=tk.Entry(aken)
+    sisestus3.pack()
    
     # Nupp, mis käivitab funktsiooni kuva_sisestus
-    nupp=tk.Button(aken,text="Vajuta mind",command=kuva_sisestus)
+    nupp=tk.Button(aken,text="Arvuta",command=kuva_sisestus)
     nupp.pack()
+
+
 
     aken.mainloop()
 
