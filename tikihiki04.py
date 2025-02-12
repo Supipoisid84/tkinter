b0e3fd14c6918e858a7477bcf6b0a987741fff82
Alex Kreimann <alex@hkhk.edu.ee>
2025-02-12 10:01:24 +0200

    Update tikihiki04.py

--- tikihiki04.py
+++ tikihiki04.py
@@ -5,17 +5,27 @@
 import tkinter as tk
 
 def main():
-    aken = tk.Tk()
-    aken.geometry("300x400")
+
+    def kuva_varv(v):
+        vastus.config(text=v)
+
+    aken=tk.Tk()
+    aken.geometry("400x100")
+    aken.title("Värvi nupud")
    
-    nupp1 = tk.Button(aken, text="Vajuta mind", bg="blue", fg="white", font=("Arial", 16))
-    nupp2 = tk.Button(aken, text="Vajuta mind", bg="black", fg="white", font=("Arial", 16))
-    nupp3 = tk.Button(aken, text="Vajuta mind", bg="white", fg="black", font=("Arial", 16))
-    nupp4 = tk.Button(aken, text="Vajuta mind", bg="green", fg="white", font=("Arial", 16))
-    nupp1.pack(padx=20, pady=10)
-    nupp2.pack(padx=20, pady=10)
-    nupp3.pack(padx=20, pady=10)
-    nupp4.pack(padx=20, pady=10)
+    nupp1=tk.Button(aken,bg="red",font=("Arial",16),command=lambda: kuva_varv("punane"))
+    nupp2=tk.Button(aken,bg="orange",font=("Arial",16),command=lambda: kuva_varv("oranž"))
+    nupp3=tk.Button(aken,bg="yellow",font=("Arial",16),command=lambda: kuva_varv("kollane"))
+    nupp4=tk.Button(aken,bg="green",font=("Arial",16),command=lambda: kuva_varv("roheline"))
+    nupp5=tk.Button(aken,bg="blue",font=("Arial",16),command=lambda: kuva_varv("sinine"))
+    vastus=tk.Label(aken,text=f"Siia tuleb vastus",font=("Arial", 16))
+    
+    vastus.pack(side="bottom",expand=True,fill="x" )
+    nupp1.pack(side="left",expand=True,fill="x" )
+    nupp2.pack(side="left",expand=True,fill="x" )
+    nupp3.pack(side="left",expand=True,fill="x" )
+    nupp4.pack(side="left",expand=True,fill="x" )
+    nupp5.pack(side="left",expand=True,fill="x" )
 
     aken.mainloop()
 
