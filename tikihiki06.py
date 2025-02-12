b2546dfa3a978ed19fc8b77bc1636502d24ef386
Alex Kreimann <alex@hkhk.edu.ee>
2025-02-12 10:01:30 +0200

    Update tikihiki06.py

--- tikihiki06.py
+++ tikihiki06.py
@@ -5,44 +5,44 @@
 import tkinter as tk
 
 aken = tk.Tk()
+aken.geometry("400x400")
 font = "Arial 10"
 padx = 5
 pady = 5
 
 nupp_00 = tk.Button(aken, text="Nupp 0,0", font=font)
-nupp_00.grid(row=0, column=0, rowspan=2, columnspan=2, padx=padx, pady=pady, sticky="nsew")
+nupp_00.grid(row=1, column=0, rowspan=5, columnspan=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_02 = tk.Button(aken, text="Nupp 0,2", font=font)
-nupp_02.grid(row=0, column=2, padx=padx, pady=pady, sticky="nsew")
+# sildid
+label = tk.Label(text="Palun sisestage oma andmed:")
+label.grid(row=0, column=2,  columnspan=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_03 = tk.Button(aken, text="Nupp 0,3", font=font)
-nupp_03.grid(row=0, column=3, padx=padx, pady=pady, sticky="nsew")
+nimi = tk.Label(text="Nimi:")
+nimi.grid(row=1, column=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_12 = tk.Button(aken, text="Nupp 1,2", font=font)
-nupp_12.grid(row=1, column=2, padx=padx, pady=pady, sticky="nsew")
+nimi = tk.Label(text="Silmade värv:")
+nimi.grid(row=2, column=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_13 = tk.Button(aken, text="Nupp 1,3", font=font)
-nupp_13.grid(row=1, column=3, padx=padx, pady=pady, sticky="nsew")
+nimi = tk.Label(text="Pikkus:")
+nimi.grid(row=3, column=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_20 = tk.Button(aken, text="Nupp 2,0", font=font)
-nupp_20.grid(row=2, column=0, padx=padx, pady=pady, sticky="nsew")
+nimi = tk.Label(text="Kaal:")
+nimi.grid(row=4, column=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_21 = tk.Button(aken, text="Nupp 2,1", font=font)
-nupp_21.grid(row=2, column=1, padx=padx, pady=pady, sticky="nsew")
+nupp_13 = tk.Button(aken, text="Salvesta", font=font)
+nupp_13.grid(row=5, column=2, padx=padx, pady=pady, sticky="nsew")
 
-nupp_22 = tk.Button(aken, text="Nupp 2,2", font=font)
-nupp_22.grid(row=2, column=2, padx=padx, pady=pady, sticky="nsew")
+# sisestused
+sis1 = tk.Entry(aken)
+sis1.grid(row=1, column=3, padx=padx, pady=pady, sticky="nsew")
 
-nupp_23 = tk.Button(aken, text="Nupp 2,3", font=font)
-nupp_23.grid(row=2, column=3, padx=padx, pady=pady, sticky="nsew")
+sis2 = tk.Entry(aken)
+sis2.grid(row=2, column=3, padx=padx, pady=pady, sticky="nsew")
 
-# Nuppude seadistamine
-aken.grid_rowconfigure(0, weight=1)
-aken.grid_rowconfigure(1, weight=1)
-aken.grid_rowconfigure(2, weight=1)
-aken.grid_columnconfigure(0, weight=1)
-aken.grid_columnconfigure(1, weight=1)
-aken.grid_columnconfigure(2, weight=1)
-aken.grid_columnconfigure(3, weight=1)
+sis3 = tk.Entry(aken)
+sis3.grid(row=3, column=3, padx=padx, pady=pady, sticky="nsew")
+
+sis4 = tk.Entry(aken)
+sis4.grid(row=4, column=3, padx=padx, pady=pady, sticky="nsew")
 
 aken.mainloop()
