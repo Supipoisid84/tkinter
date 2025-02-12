89052f7c16e6ccd13e373c7ec794bbdd20071ff7
Alex Kreimann <alex@hkhk.edu.ee>
2025-02-12 10:01:20 +0200

    Update tikihiki02.py

--- tikihiki02.py
+++ tikihiki02.py
@@ -21,22 +21,43 @@ from PIL import Image, ImageTK
     #Lisa tekstikastile kerimisriba
     #Keela akna suuruse muutmine
 
-def kuva_pilt(aken,failinimi,laius,korgus):
-    pilt=image.open(failinimi)
-    pilt=pilt.resize((laius,korgus))
-    foto=imageTK.photoimage(pilt)
-    label=tkl.label(aken,image=foto)
+def loe_fail(failinimi):
+    with open(failinimi,'r',encoding='utf-8') as file:
+        return file.read()
+
+def main():
+    aken=tk.Tk()
+    aken.title("tkinter ülesanded")
+    aken.geometry("400x400")
+    # Akna suuruse muutmise keelamine
+    aken.resizable(False,False)
+
+    # Pildi avamine ja Tkinteri jaoks ettevalmistamine
+    pilt=Image.open("Kuck Porris.webp")
+    p=0
+    pilt=pilt.crop((0+p,0+p,200+p,200+p))
+    # pilt = pilt.resize((200,200))
+    foto=ImageTk.PhotoImage(pilt)
+
+    # Sildi kuvamine
+    label=tk.Label(aken,text="Jäck Nurris",font=("Arial",16,"bold"),fg="blue").pack()
+
+    # Pildi kuvamine Label abil
+    label=tk.Label(aken,image=foto)
     label.image=foto
     label.pack()
 
-def main():
-    aken=tk.tk()
-    aken.title("Mario ülesanded")
-    aken.geometry("400x300")
+    # Tekstkast
+    tekst=tk.Text(aken,wrap=tk.WORD,font=("Arial",12))
+    scrollbar=tk.Scrollbar(aken,command=tekst.yview)
+    tekst.config(yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
+    tekst.pack(expand=True,fill=tk.BOTH)
 
-    #kuva_pilt(aken,"img/python_512x512.png",200,200)
-
+    failisisu=loe_fail("tekst.txt")
+    tekst.insert(tk.INSERT,failisisu)
+   
     aken.mainloop()
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
